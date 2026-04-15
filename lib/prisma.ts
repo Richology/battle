0b839bf6e0490eef5ff61c6e8492a7c98cfc9df4
安瑟 <panlong@AnswerdeMacBook-Pro.local>
2026-04-15 16:40:33 +0800

    feat: move prisma to supabase

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,20 @@
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient
 }
 
+const prismaOptions: Prisma.PrismaClientOptions = {
+  log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+}
+
+if (process.env.DATABASE_URL) {
+  prismaOptions.datasourceUrl = process.env.DATABASE_URL
+}
+
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
-  })
+  new PrismaClient(prismaOptions)
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma
